bf71881ebbb3e62f6362e0a79ad9a6674a9a8007
Jorge Machuca <jorge_machuca@hotmail.com>
2023-03-05 18:15:54 -0300

    added card styling

--- src/components/card/card.component.tsx
+++ src/components/card/card.component.tsx
@@ -1,25 +1,34 @@
 import Image from "next/image";
 import Link from "next/link";
 import { FC } from "react";
+import cls from "classnames";
+import styles from "./card.module.css";
 
 type CardProps = {
-    name: string;
-    imgUrl: string;
-    href: string;
+  name: string;
+  imgUrl: string;
+  href: string;
 };
 
 const Card: FC<CardProps> = (props) => {
-    return (
-        <Link href={props.href}>
-            <h2>{props.name}</h2>
-            <Image
-                src={props.imgUrl}
-                width={260}
-                height={160}
-                alt={props.name}
-            />
-        </Link>
-    );
+  return (
+    <div className={cls("glass", styles.container)}>
+      <Link className={styles.cardLink} href={props.href}>
+        <div className={styles.cardHeaderWrapper}>
+          <h2 className={styles.cardHeader}>{props.name}</h2>
+        </div>
+        <div className={styles.classImageWrapper}>
+          <Image
+            className={styles.cardImage}
+            src={props.imgUrl}
+            width={260}
+            height={160}
+            alt={props.name}
+          />
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Card;
